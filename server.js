e35bb66b6350a9b2972ce327df708f1956281109
Rafael Auyer <rafael.passos@redes.unb.br>
2018-02-06 13:03:54 -0200

    Post method

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
+    util = require('util'),
+    md5 = require('md5'),
     app = express(),
     { Client } = require('pg'),
     config = require("./config.json");
@@ -30,7 +32,7 @@ app.get("/api/servidores", (req, res) => {
     client.query(q, (err, result) => {
         if (err){
             res.status(500)
-            res.send("err")
+            res.send(err)
         } else {
             console.log("Querry Successful\n"+q)
             res.status(200)
@@ -58,36 +60,36 @@ app.post("/api/servidor/", (req, res) => {
     errorList = []
     regexCheck = false
     if (! /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/.test(req.body.data_nascimento)){
-        errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should look like this: 1969-02-12") )
+        errorList.push(new errorType("[data_nascimento] missing or failed to match API requirements. It should look like this: 1969-02-12") )
         regexCheck = true
     } else if (new Date(req.body.data_nascimento) > new Date()){
-        errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should not be in the future.") )    
+        errorList.push(new errorType("[data_nascimento] missing or failed to match API requirements. It should not be in the future.") )    
         regexCheck = true
     }
     if (!/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/.test(req.body.nome)){
         errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should not be in the future.") )    
         regexCheck = true
-    } else if (String.length(req.body.nome)>100) {
+    } else if (req.body.nome.length>100) {
         regexCheck = true
         errorList.push(new errorType("[nome] failed to match API requirements. It should have a maximum of 100 characters"))
     }
     if (!/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/.test(req.body.nome_identificacao)){
-        errorList.push(new errorType("[nome_identificacao] failed to match API requirements. It should look like this: Firstname Middlename(optional) Lastname"))
+        errorList.push(new errorType("[nome_identificacao] missing or failed to match API requirements. It should look like this: Firstname Middlename(optional) Lastname"))
         regexCheck = true
-    } else if (String.length(req.body.nome_identificacao)>100) {
+    } else if (req.body.nome_identificacao.length>100) {
         regexCheck = true
         errorList.push(new errorType("[nome_identificacao] failed to match API requirements. It should have a maximum of 100 characters"))
     }
     if (!/\b[MF]{1}\b/.test(req.body.sexo)){
         regexCheck = true
-        errorList.push(new errorType("[sexo] failed to match API requirements. It should look like this: M for male, F for female."))
+        errorList.push(new errorType("[sexo] missing or failed to match API requirements. It should look like this: M for male, F for female."))
     }
-    if (isNaN(req.body.Id_pessoa)){
-        errorList.push(new errorType("[id_pessoa] failed to match API requirements. It should be only numeric.") )    
+    if (isNaN(req.body.id_pessoa)){
+        errorList.push(new errorType("[id_pessoa] missing or failed to match API requirements. It should be only numeric.") )    
         regexCheck = true
     }
     if (isNaN(req.body.siape)){
-        errorList.push(new errorType("[siape] failed to match API requirements. It should be only numeric." )   ) 
+        errorList.push(new errorType("[siape] missing or failed to match API requirements. It should be only numeric." )   ) 
         regexCheck = true
 
     }
@@ -96,13 +98,24 @@ app.post("/api/servidor/", (req, res) => {
         res.send(errorList)
     }
 
-
-    // q=util.format(`INSERT INTO rh.servidor_tmp(
-	// 		nome, nome_identificacao, siape, id_pessoa, matricula_interna, id_foto,
-	// 		data_nascimento, sexo)
-	// 		VALUES ('%s', '%s', %d, %d, %d, null, '%s', '%s');
-	// 		`, req.body.Nome, req.body.Nomeidentificacao, req.body.Siape, req.body.Idpessoa, req.body.Matriculainterna,
-	// 	req.body.Datanascimento, req.body.Sexo)
+    bid = parseInt(md5(req.body.nome_identificacao+new Date().toString()), 16) % 999999;
+    q= util.format(`INSERT INTO rh.servidor_tmp(
+			nome, nome_identificacao, siape, id_pessoa, matricula_interna, id_foto,
+			data_nascimento, sexo)
+			VALUES ('%s', '%s', %d, %d, %d, null, '%s', '%s');
+			`, req.body.nome, req.body.nome_identificacao, req.body.siape, req.body.id_pessoa, bid,
+             req.body.data_nascimento, req.body.sexo)
+    client.query(q, (err, result) => {
+        if (err){
+            res.status(500)
+            res.send(err)
+        } else {
+            console.log("Querry Successful\n"+q)
+            res.header( "location" , "https://"+req.hostname+"/api/servidor/"+bid)
+            res.status(201)
+            res.send() 
+        }
+    })
 })
 
 app.listen(config.HttpPort);
