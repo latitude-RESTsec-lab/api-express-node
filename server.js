45b7732295ef8e98e52b7ff6fce93f27272c9e7c
Auyer <rafa_auyer@icloud.com>
2018-02-10 15:01:46 -0200

    regex checking

--- server.js
+++ server.js
@@ -60,41 +60,49 @@ app.post("/api/servidor/", (req, res) => {
     if (! /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/.test(req.body.data_nascimento)){
         errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should look like this: 1969-02-12") )
         regexCheck = true
-    } else if (false){
+    } else if (new Date(req.body.data_nascimento) > new Date()){
+        errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should not be in the future.") )    
         regexCheck = true
     }
     if (!/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/.test(req.body.nome)){
+        errorList.push(new errorType("[data_nascimento] failed to match API requirements. It should not be in the future.") )    
         regexCheck = true
-    } else if (false) {
+    } else if (String.length(req.body.nome)>100) {
         regexCheck = true
+        errorList.push(new errorType("[nome] failed to match API requirements. It should have a maximum of 100 characters"))
     }
     if (!/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/.test(req.body.nome_identificacao)){
+        errorList.push(new errorType("[nome_identificacao] failed to match API requirements. It should look like this: Firstname Middlename(optional) Lastname"))
         regexCheck = true
-    } else if (false) {
+    } else if (String.length(req.body.nome_identificacao)>100) {
         regexCheck = true
+        errorList.push(new errorType("[nome_identificacao] failed to match API requirements. It should have a maximum of 100 characters"))
     }
     if (!/\b[MF]{1}\b/.test(req.body.sexo)){
         regexCheck = true
+        errorList.push(new errorType("[sexo] failed to match API requirements. It should look like this: M for male, F for female."))
     }
-    // if (!//.test(req.body.)){
-    // }
-    // if (!//.test(req.body.)){
-    // }
-    // if (!//.test(req.body.)){
-    // }
-    // if (!//.test(req.body.)){
-    // }
+    if (isNaN(req.body.Id_pessoa)){
+        errorList.push(new errorType("[id_pessoa] failed to match API requirements. It should be only numeric.") )    
+        regexCheck = true
+    }
+    if (isNaN(req.body.siape)){
+        errorList.push(new errorType("[siape] failed to match API requirements. It should be only numeric." )   ) 
+        regexCheck = true
 
+    }
+    if (regexCheck){
+        res.status(400)
+        res.send(errorList)
+    }
 
-    
-    res.send(errorList)
 
     // q=util.format(`INSERT INTO rh.servidor_tmp(
 	// 		nome, nome_identificacao, siape, id_pessoa, matricula_interna, id_foto,
 	// 		data_nascimento, sexo)
 	// 		VALUES ('%s', '%s', %d, %d, %d, null, '%s', '%s');
-	// 		`, ser.Nome, ser.Nomeidentificacao, ser.Siape, ser.Idpessoa, ser.Matriculainterna,
-	// 	ser.Datanascimento, ser.Sexo)
+	// 		`, req.body.Nome, req.body.Nomeidentificacao, req.body.Siape, req.body.Idpessoa, req.body.Matriculainterna,
+	// 	req.body.Datanascimento, req.body.Sexo)
 })
 
 app.listen(config.HttpPort);
